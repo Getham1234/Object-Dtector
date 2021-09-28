8d152731996eefc8990f25e0753f94165ec98923
Getham1234 <73149122+Getham1234@users.noreply.github.com>
2021-09-28 16:47:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if (status!=""){
+        document.getElementById("status").innerHTML = "Objects Detected";
+        for(i=0;i<objects.length;i++){
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
